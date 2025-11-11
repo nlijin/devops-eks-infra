47350daa97a34e767c4ad57aba6e5c79db08c9e6
nlijin <nlijinraj@gmail.com>
2025-11-11 19:24:50 +0530

    replaced use_lockfile key with dynamodb_table

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "devops-eks-terraform-state-lijin"
-    key          = "eks/terraform.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = true
-    encrypt      = true
+    bucket         = "devops-eks-terraform-state-lijin"
+    key            = "eks/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
   }
 }
